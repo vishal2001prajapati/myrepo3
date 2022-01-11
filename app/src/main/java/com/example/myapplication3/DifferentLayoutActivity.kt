60886613e613b88f7fd6f9caa03ffa084ceaeeef
vishal prajapati <vishal.prajapati@simformsolutions.com>
2022-01-11 11:46:59 +0530

    TE5-T375 chapter3(A) Complete

---
+++ app/src/main/java/com/example/myapplication3/DifferentLayoutActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DifferentLayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_different_layout)
+    }
+}
--- app/src/main/java/com/example/myapplication3/MainActivity.kt
+++ app/src/main/java/com/example/myapplication3/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.myapplication3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var button: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button = findViewById(R.id.button_ok)
+        button.setOnClickListener {
+            Toast.makeText(this, "This is a OK Button", Toast.LENGTH_LONG).show()
+        }
+        val toast: TextView = findViewById(R.id.textview)
+        toast.setOnClickListener {
+            showtoast()
+        }
+    }
+
+    private fun showtoast() {
+        val inflater: LayoutInflater = layoutInflater
+        val view: View = inflater.inflate(R.layout.custom_toast, findViewById(R.id.custom))
+        val toast: Toast = Toast(this)
+        toast.setGravity(Gravity.BOTTOM or Gravity.FILL_HORIZONTAL, 0, 0)
+        toast.duration = Toast.LENGTH_LONG
+        toast.view = view
+        toast.show()
     }
 }
