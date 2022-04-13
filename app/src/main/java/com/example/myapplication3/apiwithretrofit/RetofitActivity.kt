07f0c8dfc050a6e13c7d2519c1ff0d123846645f
vishal prajapati <vishal.prajapati@simformsolutions.com>
2022-04-13 15:39:45 +0530

    retofit files added

---
+++ app/src/main/java/com/example/myapplication3/apiwithretrofit/RetofitActivity.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication3.apiwithretrofit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication3.R
+
+class RetofitActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_retofit)
+
+    }
+}
